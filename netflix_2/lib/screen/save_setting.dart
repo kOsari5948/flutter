89dd53af21b4aa9aedbd21f9f382c8e7b14480c3
kOsari5948 <kmc5948@naver.com>
2023-01-10 15:45:57 +0900

    20230110

---
+++ netflix_2/lib/screen/save_setting.dart
@@ -0,0 +1,103 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:netflix_2/screen/save_setting.dart';
+import 'package:netflix_2/widget/bottom_bar.dart';
+
+class save_setting extends StatefulWidget {
+  @override
+  State<save_setting> createState() => _save_setting(); //상태 줘서 내부 만들기
+}
+
+class _save_setting extends State<save_setting> {
+  @override
+  Widget build(BuildContext context) {
+    //위젯 시작
+    return MaterialApp(
+      //머테리얼(안드로이드 스타일)로 만들꺼야
+      title: 'Bbongflix',
+      debugShowCheckedModeBanner: false, //디버그 포장 없에기
+
+      theme: ThemeData(
+        //테마 설정
+        scaffoldBackgroundColor: Colors.black,
+        backgroundColor: Colors.black,
+        brightness: Brightness.dark, //전체 색상
+        primaryColor: Colors.black, //타이틀 색상
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          actions: <Widget>[
+            new IconButton(
+              icon: new Icon(
+                Icons.cancel,
+                color: Color.fromARGB(255, 35, 35, 35),
+                size: 35,
+              ),
+              tooltip: 'profile',
+              onPressed: () => {Navigator.pop(context)},
+            ),
+          ],
+          toolbarHeight: 70,
+          flexibleSpace: new Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: FractionalOffset.topCenter,
+                    end: FractionalOffset.bottomCenter,
+                    colors: [
+                  Color.fromARGB(255, 0, 0, 0),
+                  Color.fromARGB(20, 0, 0, 0),
+                ],
+                    stops: [
+                  0,
+                  1
+                ])),
+          ),
+        ),
+        //화면 그리기
+        body: ListView(
+          children: <Widget>[
+            //icon 버튼은 사진만 들어감 text에다가 icon달아야함
+
+            Column(children: <Widget>[
+              Container(
+                  padding: EdgeInsets.only(top: 50),
+                  child: Text(
+                    "시작해 볼까요?",
+                    style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                  )),
+              Container(
+                padding: EdgeInsets.only(top: 10),
+                width: 300,
+                child: Text(
+                  "몇 GB의 영화의 시리즈를 저장하고 싶은지 선택하세요. '설정'> '스마트 저장' 메뉴에서 언제든지 변경할 수 있습니다.",
+                  style: TextStyle(fontSize: 13.5, color: Colors.grey),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                child: Image.asset("src/image/save.jpg"),
+                height: 300,
+                padding: EdgeInsets.only(bottom: 50),
+              ),
+              //아웃라인 버튼 테두리 있는 버튼
+              //ElevatedButton 색있음
+              Container(
+                width: 280,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text("설정하기"),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 81, 104, 220),
+                      foregroundColor: Colors.white,
+                      textStyle: TextStyle(fontWeight: FontWeight.bold)),
+                ),
+              ),
+            ]),
+          ],
+        ),
+      ),
+    );
+  }
+}
