87f869b5b63759a558f25b19df4d9bed3a7146d8
leafeafeaf <rlatjdals369@naver.com>
2023-02-09 16:38:49 +0900

    20230209_2

--- netflix_2/lib/screen/search_screen.dart
+++ netflix_2/lib/screen/search_screen.dart
@@ -33,35 +33,69 @@ class _SearchScreenState extends State<SearchScreen> {
 
   Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
     List<DocumentSnapshot> searchResults = [];
+    List<Widget> wlist = [];
     for (DocumentSnapshot d in snapshot) {
       if (d.data().toString().contains(_searchText)) {
         //검색어 포함 찾기
         searchResults.add(d);
       }
     }
+
     return Expanded(
-        child: GridView.count(
-      crossAxisCount: 3,
-      childAspectRatio: 1 / 1.5,
-      padding: EdgeInsets.all(3),
-      children:
-          searchResults.map((data) => _buildListItem(context, data)).toList(),
-      //children:searchResults.map((data)=>_buildListItem(context, snapshot)),
-    ));
+        child: ListView(padding: EdgeInsets.fromLTRB(3, 10, 3, 10), children: [
+      Container(
+        padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
+        child: Text(
+          "최다 검색",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+      Column(
+        children:
+            searchResults.map((data) => _buildListItem(context, data)).toList(),
+      )
+    ]));
   }
 
   Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
     final movie = Movie.fromSnapShot(data);
-    return InkWell(
-      child: Image.network(movie.img!),
-      onTap: () {
-        Navigator.of(context).push(MaterialPageRoute<Null>(
-            fullscreenDialog: true,
-            builder: (BuildContext context) {
-              return Container(); //여기에 상세화면을 띄워라 @@@
-            }));
-      },
+    return Container(
+      margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
+      child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        InkWell(
+          child: Row(
+            children: <Widget>[
+              Image.network(
+                movie.img!,
+                width: 160,
+                height: 80,
+                fit: BoxFit.cover,
+              ),
+              Container(
+                width: 100,
+                margin: EdgeInsets.all(5),
+                child: Text(movie.title!, maxLines: 8),
+              )
+            ],
+          ),
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute<Null>(
+                fullscreenDialog: true,
+                builder: (BuildContext context) {
+                  return Container(); //여기에 상세화면을 띄워라 @@@
+                }));
+          },
+        ),
+        Icon(
+          Icons.play_circle_outline,
+          size: 50,
+        )
+      ],
+    ),
     );
+    
   }
 
   @override
@@ -78,14 +112,36 @@ class _SearchScreenState extends State<SearchScreen> {
         ),
         home: Scaffold(
           body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Padding(padding: EdgeInsets.all(30)),
-
+              Padding(padding: EdgeInsets.all(10)),
+              //검색창
               Container(
                 color: Colors.black,
                 padding: EdgeInsets.fromLTRB(5, 10, 5, 10),
                 child: Row(
                   children: <Widget>[
+                    focusNode.hasFocus
+                        ? Expanded(
+                            child: TextButton(
+                              child: Icon(
+                                Icons.arrow_back_ios,
+                                size: 20,
+                                color: Colors.white,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _filter.clear();
+                                  _searchText = "";
+                                  focusNode.unfocus();
+                                });
+                              },
+                            ),
+                          )
+                        : Expanded(
+                            flex: 0,
+                            child: Container(),
+                          ),
                     Expanded(
                       flex: 6,
                       child: TextField(
@@ -132,27 +188,10 @@ class _SearchScreenState extends State<SearchScreen> {
                         ),
                       ),
                     ),
-                    focusNode.hasFocus
-                        ? Expanded(
-                            child: TextButton(
-                              child: Text('취소'),
-                              onPressed: () {
-                                setState(() {
-                                  _filter.clear();
-                                  _searchText = "";
-                                  focusNode.unfocus();
-                                });
-                              },
-                            ),
-                          )
-                        : Expanded(
-                            flex: 0,
-                            child: Container(),
-                          )
                   ],
                 ),
               ),
-
+              //검색된 요소
               _buildBody(context),
             ],
           ),
